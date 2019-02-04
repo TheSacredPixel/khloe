19e2b86592e9dc9a05fe96341766f9ecd17b936d
Kimon Kar <kimon.kar@gmail.com>
2019-02-04 21:25:10 +0200

    k NOW I understand

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -2,10 +2,10 @@ const { Command } = require('discord-akairo'),
 	{ Attachment } = require('discord.js'),
 	{ createCanvas, loadImage } = require('canvas')
 
-class WhoAmICommand extends Command {
+class ProfileCommand extends Command {
 	constructor() {
-		super('whoami', {
-			aliases: ['whoami'],
+		super('profile', {
+			aliases: ['profile', 'character', 'char'],
 			description: ''
 		})
 	}
@@ -13,22 +13,25 @@ class WhoAmICommand extends Command {
 	async exec(msg) {
 		let get = this.client.provider.get(msg.author.id, 'identity', null)
 
-		if(!get)
+		if(!get)// TODO: add @ping resolution, as well as char name search
 			return msg.util.send('I don\'t know :( Use `>iam (character name) (server)` so that I learn!')
 
-		return msg.util.send(`You're ${get.name} of ${get.server}!`)
+		//return msg.util.send(`You're ${get.name} of ${get.server}!`)
 
-		const char = await this.client.xiv.character.get(get.id)
+		const res = await this.client.xiv.character.get(get.id, { extended: true })
+
+		if(!res.status.ok)
+			return msg.util.send('Error: Status was NOT ok')//PH
 
 		const canvas = createCanvas(640, 873)
 		const ctx = canvas.getContext('2d')
 
-		const img = await loadImage(char.portrait)
+		const img = await loadImage(res.character.portrait)
 		ctx.drawImage(img, 0, 0)
 
 		const attach = new Attachment(canvas.toBuffer(), 'test.png')
-		return msg.util.send('Here: ', attach)
+		return msg.util.send('', attach)
 	}
 }
 
-module.exports = WhoAmICommand
+module.exports = ProfileCommand
--- test.js
+++ test.js
@@ -1,19 +1,24 @@
-const { createCanvas, loadImage } = require('canvasa'),
-	fs = require('fs'),
-	req = require('request-promise-native')
+const { createCanvas, loadImage } = require('canvas'),
+	fs = require('fs')
 
 const run = async () => {
 	try {
-		const canvas = createCanvas(640, 873)
+		const canvas = createCanvas(950, 873)
 		const ctx = canvas.getContext('2d')
 
 
 		const out = fs.createWriteStream(__dirname + '/test.png')
-		return req('https://img2.finalfantasyxiv.com/f/b7ce37b6c39be01edf5b6d8048e6dd34_745baffc465480ed372e274d50318290fl0_640x873.jpg').pipe(fs.createWriteStream('test.png'))
-		const img = await loadImage(get)
-		ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+		//req('https://img2.finalfantasyxiv.com/f/b7ce37b6c39be01edf5b6d8048e6dd34_745baffc465480ed372e274d50318290fl0_640x873.jpg').pipe(out)
+		//let get = 'https://img2.finalfantasyxiv.com/f/b7ce37b6c39be01edf5b6d8048e6dd34_745baffc465480ed372e274d50318290fl0_640x873.jpg'
+		const img = await loadImage('./img.jpg')
+		ctx.drawImage(img, 310, 0)
 
+		ctx.fillRect(0,0,400,873)
 
+		ctx.font = '42px Meiryo'
+		ctx.fillStyle = 'white'
+		ctx.textAlign = 'center'
+		ctx.fillText('Kai Megumi', 200, 100, 300)
 
 		const stream = canvas.createPNGStream()
 		await stream.pipe(out)
